5402e7ca55f1deb9f3dbb3b2cd96acd9c41e2876
LenaMazurenko <lena@rent-it.su>
2021-10-04 19:51:12 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+let liItem;
+const listOfIngredients = document.querySelector("#ingredients");
+
+for (let item of ingredients) {
+    liItem = document.createElement("li");
+    liItem.textContent = item;
+    listOfIngredients.append(liItem);
+    liItem.setAttribute("class", "item");
+}
