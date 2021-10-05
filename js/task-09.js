6cba7cc0f20f7733d8fa2e1714d7729e99c85f91
LenaMazurenko <lena@rent-it.su>
2021-10-05 15:39:31 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const str = document.querySelector(".color");
+let actualColor;
+btn.addEventListener("click", () => {
+    actualColor = getRandomHexColor();
+    str.textContent = actualColor;
+    body.style.backgroundColor = actualColor;
+});
