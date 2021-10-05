8787ebeaaea743a92f98b768f92bdb7e9ac60700
LenaMazurenko <lena@rent-it.su>
2021-10-05 15:15:38 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "")
+        alert("Необходимо заполнить все поля формы!");
+    else console.log(`email: ${email.value}, passvord: ${password.value}`);
+    event.currentTarget.reset();
+}
