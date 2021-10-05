73b1e33a3659b9255a0e9f98428eae7a0f1b9da3
LenaMazurenko <lena@rent-it.su>
2021-10-05 23:09:03 +0300

    task-10

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputControl = document.querySelector("#font-size-control");
 const outputText = document.querySelector("#text");
+
 inputControl.addEventListener("change", (value) => {
     outputText.style.fontSize = `${inputControl.value}px`;
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector("#controls input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const containerForBoxes = document.querySelector("#boxes");
+
+let activeNumber;
+
+function createBoxes(currentValue) {
+    let divItem;
+    for (let i = 0; i < currentValue; i += 1) {
+        divItem = document.createElement("div");
+        divItem.style.backgroundColor = getRandomHexColor();
+        divItem.style.width = `${30 + 10 * i}px`;
+        divItem.style.height = `${30 + 10 * i}px`;
+
+        containerForBoxes.append(divItem);
+    }
+}
+
+inputNumber.addEventListener("change", (value) => {
+    activeNumber = inputNumber.value;
+    console.log(activeNumber);
+});
+
+btnCreate.addEventListener("click", () => {
+    createBoxes(activeNumber);
+});
+
+btnDestroy.addEventListener("click", () => {
+    document.querySelector("#boxes").remove();
+});
