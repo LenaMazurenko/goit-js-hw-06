4c4f6d23c4dee504864eefbe63d864ce0225e257
LenaMazurenko <lena@rent-it.su>
2021-10-06 15:33:36 +0300

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -2,12 +2,7 @@ const arrayOfCategories = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${arrayOfCategories.length}`);
 
-let nameCategories;
-let numberOfItems;
-for (let i = 0; i < arrayOfCategories.length; i += 1) {
-    nameCategories = arrayOfCategories[i].querySelector("h2").textContent;
-    console.log(`Category: ${nameCategories}`);
-
-    numberOfItems = arrayOfCategories[i].querySelectorAll("li").length;
-    console.log(`Elements: ${numberOfItems}`);
-}
+arrayOfCategories.forEach((item) => {
+    console.log(`Category: ${item.querySelector("h2").textContent}`);
+    console.log(`Elements: ${item.querySelectorAll("li").length}`);
+});
